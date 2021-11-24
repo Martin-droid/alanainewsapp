b311d74fc911a36fd6914d06f522aa1d930cf9b8
Martin-droid <martmnsh@gmail.com>
2021-11-24 19:01:00 +0300

    first commit

---
+++ build/precache-manifest.d13dea29f25fc5b0970fd9c9ae2731d1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bd83f05e7e84ecab9fe32cae1a99e25",
+    "url": "/index.html"
+  },
+  {
+    "revision": "253c796fea3839c65ddc",
+    "url": "/static/css/2.ef2319b8.chunk.css"
+  },
+  {
+    "revision": "98c8300dbd6f8f2fdbdb",
+    "url": "/static/css/main.3c2fef4c.chunk.css"
+  },
+  {
+    "revision": "253c796fea3839c65ddc",
+    "url": "/static/js/2.21388a7a.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/static/js/2.21388a7a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98c8300dbd6f8f2fdbdb",
+    "url": "/static/js/main.e82d000e.chunk.js"
+  },
+  {
+    "revision": "2a5e9718c304aba39f84",
+    "url": "/static/js/runtime-main.0f5541c3.js"
+  },
+  {
+    "revision": "940c8a35294e4ebbc0786f586aba5f8e",
+    "url": "/static/media/logo1.940c8a35.png"
+  }
+]);
